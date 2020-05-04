c1173839ddbbe075db6ebda1314f893137631adf
Yehuda Neufeld <newgraphil@gmail.com>
2020-05-04 11:50:16 +0300

    added forgotten file...

---
+++ resources/views/components/head-meta.blade.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#ffad1f">
+
+<meta property="og:title" content="Yiddishe Kop Blog">
+<meta name="Description" content="Articles about programming and software development.">
+<meta property="og:description" content="Articles about programming and software development.">
+<meta property="og:image" content="https://blog.yiddishe-kop.com/assets/images/cover.jpg">
+<meta property="og:url" content="https://blog.yiddishe-kop.com/">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Yiddishe Kop Blog">
+<meta name="twitter:description" content="Articles about programming and software development.">
+<meta name="twitter:image" content="https://blog.yiddishe-kop.com/assets/images/cover.jpg">
+
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+@stack('head')
